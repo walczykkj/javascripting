5ab260a698cc339b99b7b62a77a2d84870d3f050
Karolina Walczyk <37847921+walczykkj@users.noreply.github.com>
2018-06-19 15:15:35 -0400

    Add files via upload

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+var total = 0;
+var limit = 45;
+ for (var i = 0; i > limit; i++) {
+
+       total+= i;
+     }
+
+     console.log(total);
+
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+roundUp = 1.5;
+var rounded = Math.round(roundUp);
+console.log(rounded);
